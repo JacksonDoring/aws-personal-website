55f25ce11776afb21e87bd2b9f0d071d97834438
unknown <jackson.doring@outlook.com>
2020-07-06 14:21:07 +1000

    finished working on the education section and started working on the skills section

---
+++ src/Components/Skills-bar.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function SkillsBar(text: string) {
+  return <p>{text}</p>;
+}
+
+export default SkillsBar;
--- src/Pages/Resume.tsx
+++ src/Pages/Resume.tsx
@@ -1,77 +1,80 @@
-import React from 'react';
-import '../Style/resume.css'
-import Container from '@material-ui/core/Container';
-import CalendarTodayIcon from '@material-ui/icons/CalendarToday';
-import HomeIcon from '@material-ui/icons/Home';
-import Grid from '@material-ui/core/Grid';
-import PhoneIcon from '@material-ui/icons/Phone';
-import EmailIcon from '@material-ui/icons/Email';
-import QUT from '../Components/QUT';
+import React from "react";
+import "../Style/resume.css";
+import Container from "@material-ui/core/Container";
+import CalendarTodayIcon from "@material-ui/icons/CalendarToday";
+import HomeIcon from "@material-ui/icons/Home";
+import Grid from "@material-ui/core/Grid";
+import PhoneIcon from "@material-ui/icons/Phone";
+import EmailIcon from "@material-ui/icons/Email";
+import QUT from "../Components/QUT";
+import SkillsBar from "../Components/Skills-bar";
 
-function ContactInformation(){
-    return(
-        <Container maxWidth="lg" className="info-container">
-            <h1>Contact Information</h1>
+function ContactInformation() {
+  return (
+    <Container maxWidth="lg" className="info-container">
+      <h1>Contact Information</h1>
+      <div className="birthday">
+        <Grid container>
+          <Grid item xs={6}>
             <div className="birthday">
-                <Grid container>
-                    <Grid item xs={6}>
-                        <div className="birthday">
-                            <CalendarTodayIcon fontSize="large" className="icon"/>
-                            <p className="contact-info">02/09/2000</p>
-                        </div>
-                    
-                    </Grid>
-
-                    <Grid item xs={6} className="last-icon">
-                        <div className="birthday">
-                            <HomeIcon fontSize="large" className="icon"/>
-                            <p className="contact-info">10 Meadows Drive, Kallangur</p>
-                        </div>      
-                    </Grid>
-
-                    <Grid item xs={6}>
-                        <div className="birthday">
-                            <PhoneIcon fontSize="large" className="icon"/>
-                            <p className="contact-info">0401 429 998</p>
-                        </div>
-                    </Grid>
-
-                    <Grid item xs={6} className="last-icon">
-                        <div className="birthday">
-                            <EmailIcon fontSize="large" className="icon"/>
-                            <p className="contact-info">jackson_doring@outlook.com</p>
-                        </div>      
-                    </Grid>
-
-                    <Grid item xs={12}>
-                        <a href="https://github.com/JacksonDoring" id="website-link">https://github.com/JacksonDoring</a>
-                    </Grid>
-                </Grid>
+              <CalendarTodayIcon fontSize="large" className="icon" />
+              <p className="contact-info">02/09/2000</p>
             </div>
-        </Container>
-    );
+          </Grid>
+
+          <Grid item xs={6} className="last-icon">
+            <div className="birthday">
+              <HomeIcon fontSize="large" className="icon" />
+              <p className="contact-info">10 Meadows Drive, Kallangur</p>
+            </div>
+          </Grid>
+
+          <Grid item xs={6}>
+            <div className="birthday">
+              <PhoneIcon fontSize="large" className="icon" />
+              <p className="contact-info">0401 429 998</p>
+            </div>
+          </Grid>
+
+          <Grid item xs={6} className="last-icon">
+            <div className="birthday">
+              <EmailIcon fontSize="large" className="icon" />
+              <p className="contact-info">jackson_doring@outlook.com</p>
+            </div>
+          </Grid>
+
+          <Grid item xs={12}>
+            <a href="https://github.com/JacksonDoring" id="website-link">
+              https://github.com/JacksonDoring
+            </a>
+          </Grid>
+        </Grid>
+      </div>
+    </Container>
+  );
 }
 
-function AboutMe(){
-    return(
-        <Container maxWidth="lg" className="info-container">
-            <h1>About Me</h1>
+function AboutMe() {
+  return (
+    <Container maxWidth="lg" className="info-container">
+      <h1>About Me</h1>
 
-            <div className="about-text">
-                <p>I am a hard working person</p>
-            </div>
-        </Container>
-    );
+      <div className="about-text">
+        <p>I am a hard working person</p>
+      </div>
+    </Container>
+  );
 }
 
 function Resume() {
-    return (
-      <div className="App">
-          <ContactInformation/>
-          <AboutMe/>
-          <QUT/>
-      </div>
-    );
-  }
-  
-  export default Resume;
+  return (
+    <div className="App">
+      <ContactInformation />
+      <AboutMe />
+      <QUT />
+      {SkillsBar("testing")}
+    </div>
+  );
+}
+
+export default Resume;
